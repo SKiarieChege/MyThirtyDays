d4d3e4625f77aab4bba31514b461403d74008a71
Stephen Chege <Stephen.Chege@strathmore.edu>
2022-07-27 16:23:20 +0300

    Add project files.

---
+++ MyThirtyDays/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyThirtyDays.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ MyThirtyDays/Models/TestDB_AuthStoreContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace MyThirtyDays.Models
+{
+    public partial class TestDB_AuthStoreContext : DbContext
+    {
+        public TestDB_AuthStoreContext()
+        {
+        }
+
+        public TestDB_AuthStoreContext(DbContextOptions<TestDB_AuthStoreContext> options)
+            : base(options)
+        {
+        }
+
+        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}

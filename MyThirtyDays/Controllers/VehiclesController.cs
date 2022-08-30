7d962fefdd460f718a961e46ee23c9bbe480a346
Stephen Chege <Stephen.Chege@strathmore.edu>
2022-08-30 23:02:22 +0300

    Updated The Vehicles Controller and  Vehicles DAO with functiional CRUD functionalities

--- MyThirtyDays/Controllers/VehiclesController.cs
+++ MyThirtyDays/Controllers/VehiclesController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyThirtyDays.Models;
+using MyThirtyDays.Services;
 
 namespace MyThirtyDays.Controllers
 {
@@ -6,7 +8,58 @@ namespace MyThirtyDays.Controllers
     {
         public IActionResult Index()
         {
+            VehiclesDAO vahicles = new VehiclesDAO();
             return View();
         }
+
+        public IActionResult ShowDetails(int VehicleId)
+        {
+            VehiclesDAO vehicles = new VehiclesDAO();
+            VehiclesModel vehicle = vehicles.GetVehicleById(VehicleId);
+
+            return View(vehicle);
+        }
+
+        public IActionResult CreateVehicle()
+        {
+            return View();
+        }
+
+        public IActionResult ProcessCreate(VehiclesModel vehicle)
+        {
+            VehiclesDAO vehicles = new VehiclesDAO();
+            vehicles.Insert(vehicle);
+
+            return View("Index", vehicles.GetAllVehicles());
+
+        }
+
+        public IActionResult Edit(int VehicleId)
+        {
+            VehiclesDAO vehicles = new VehiclesDAO();
+            VehiclesModel vehicle = vehicles.GetVehicleById(VehicleId);
+
+            return View("VehicleEditForm", vehicle);
+        }
+
+        public IActionResult ProcessEdit(VehiclesModel vehicle)
+        {
+            VehiclesDAO vehicles = new VehiclesDAO();
+            vehicles.Update(vehicle);
+
+            return View("Index", vehicles.GetAllVehicles());
+
+        }
+
+        public IActionResult Delete(int VehicleId)
+        {
+            VehiclesDAO vehicles = new VehiclesDAO();
+            VehiclesModel vehicle = vehicles.GetVehicleById(VehicleId);
+            vehicles.Delete(vehicle);
+
+            return View("Index", vehicles.GetAllVehicles());
+
+        }
+
     }
 }
--- MyThirtyDays/Services/VehiclesDAO.cs
+++ MyThirtyDays/Services/VehiclesDAO.cs
@@ -6,9 +6,31 @@ namespace MyThirtyDays.Services
     public class VehiclesDAO : IVehicleDataService
     {
         string connectionString = @"Data Source=SChege;Initial Catalog=MyThirtyDays;User ID=sa;Password=myadmin01?;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-        public int Delete(VehiclesModel Vehicle)
+        public int Delete(VehiclesModel vehicle)
         {
-            throw new NotImplementedException();
+            int NewIdNumber = -1;
+
+            string sqlStatement = "DELETE FROM DBO.Vehicles WHERE Id = @Id";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(sqlStatement, connection);
+
+                command.Parameters.AddWithValue("@Id", vehicle.VehicleId);
+
+                try
+                {
+                    connection.Open();
+
+                    NewIdNumber = Convert.ToInt32(command.ExecuteScalar());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+            return NewIdNumber;
         }
 
         public List<VehiclesModel> GetAllVehicles()
@@ -42,12 +64,72 @@ namespace MyThirtyDays.Services
 
         public VehiclesModel GetVehicleById(int VehicleId)
         {
-            throw new NotImplementedException();
+            VehiclesModel foundVehicle = null;
+
+            string sqlStatement = "SELECT * FROM DBO.Vehicles WHERE Id = @Id";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(sqlStatement, connection);
+                command.Parameters.AddWithValue("@Id", VehicleId);
+
+                try
+                {
+                    connection.Open();
+
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        foundVehicle = new VehiclesModel
+                        {
+                            VehicleId = (int)reader[0],
+                            VehicleMake = (string)reader[1],
+                            VehicleModel= (string)reader[1],
+                            VehicleCapacity = (decimal)reader[2],
+                            VehicleRegistrationPlate = (string)reader[3]
+                        };
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+            return foundVehicle;
         }
 
-        public int Insert(VehiclesModel Vehicle)
+        public int Insert(VehiclesModel vehicle)
         {
-            throw new NotImplementedException();
+            int NewIdNumber = -1;
+
+            string sqlStatement = "INSERT INTO DBO.Vehicles(VehicleMake,VehicleModel,VehicleCapacity,VehicleRegistrationPlate)" +
+                                    "SELECT  VehicleMake = @Make, VehicleModel = @Model, " +
+                                    "VehicleCapacity = @Capacity, VehicleRegistrationPlate= @Registration";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(sqlStatement, connection);
+                command.Parameters.AddWithValue("@Make", vehicle.VehicleMake);
+                command.Parameters.AddWithValue("@Model", vehicle.VehicleModel);
+                command.Parameters.AddWithValue("@Capacity", vehicle.VehicleCapacity);
+                command.Parameters.AddWithValue("@Registration", vehicle.VehicleRegistrationPlate);
+
+
+                try
+                {
+                    connection.Open();
+
+                    NewIdNumber = Convert.ToInt32(command.ExecuteScalar());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+            return NewIdNumber;
         }
 
         public List<VehiclesModel> SearchVehicles(string SearchTerm)
@@ -55,9 +137,35 @@ namespace MyThirtyDays.Services
             throw new NotImplementedException();
         }
 
-        public int Update(VehiclesModel Vehicle)
+        public int Update(VehiclesModel vehicle)
         {
-            throw new NotImplementedException();
+            int NewIdNumber = -1;
+
+            string sqlStatement = "UPDATE DBO.Vehicles SET VehicleMake = @Make, VehicleModel = @Model, " +
+                                    "VehicleCapacity = @Capacity, VehicleRegistrationPlate= @Registration";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(sqlStatement, connection);
+                command.Parameters.AddWithValue("@Make", vehicle.VehicleMake);
+                command.Parameters.AddWithValue("@Model", vehicle.VehicleModel);
+                command.Parameters.AddWithValue("@Capacity", vehicle.VehicleCapacity);
+                command.Parameters.AddWithValue("@Registration", vehicle.VehicleRegistrationPlate);
+
+
+                try
+                {
+                    connection.Open();
+
+                    NewIdNumber = Convert.ToInt32(command.ExecuteScalar());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+            return NewIdNumber;
         }
     }
 }
